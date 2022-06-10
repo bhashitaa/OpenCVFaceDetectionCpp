27cfad1629b36863fcaf231f593009caaca0d87a
Bhashita Sahu <88206232+bhashitaa@users.noreply.github.com>
2022-06-10 22:54:48 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,66 +1,85 @@
-
-
-#include <opencv2/objdetect/objdetect.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
-#include <iostream>
-#include <cstdio>
+#include<iostream>
+#include<stdlib.h>
+#include<string.h>
+#include<opencv2\highgui\highgui.hpp>
+#include<opencv2\imgproc\imgproc.hpp>
+#include<opencv2\objdetect\objdetect.hpp>
 
 using namespace std;
 using namespace cv;
 
-#define ESC 27
+void detectFaces(Mat img, char output_path[]);
+CascadeClassifier faceDetection;
+char input_path[100];
 
-int main( int argc, char** argv)
-{
-	CvCapture* capture;
-	Mat frame, frame_gray;
-	vector<Rect> faces;
-	CascadeClassifier face_cascade;
-	int c, i;
-	int vid_dev = 0;/* default device */
 
-	for(i = 1; i < argc ; ++i) {
-		if(strcmp(argv[i], "-d") == 0) {
-			vid_dev = atoi(argv[++i]);
-		}
+int main() {
+
+	if (!faceDetection.load("C:\\Users\\Lenovo\\Downloads\\opencv\\sources\\data\\haarcascades\\haarcascade_frontalface_default.xml")) {
+		cout << "\n XML File not found";
+		exit(0);
 	}
 
-	/* frontal face detection */
-	if(!face_cascade.load("data/face_detect_frontal.xml")) {
-		return -1;
+	char img1[40], img2[40];
+
+	cout << "\n Enter name of first image : ";
+	cin.getline(img1, 40);   // first image
+	 
+	cout << "\n Enter name of  Second image : ";
+	cin.getline(img2, 40);   // second image
+
+
+	strcpy(input_path, "input\\");
+	strcat(input_path, img1);
+
+	Mat image1 = imread(input_path, CV_LOAD_IMAGE_UNCHANGED);
+
+	strcpy(input_path, "input\\");
+	strcat(input_path, img2);
+
+	Mat image2 = imread(input_path, CV_LOAD_IMAGE_UNCHANGED);
+
+	if (image1.empty()) {
+		cout << "\n image is not loaded! - " << img1;
+	}
+	
+	if (image2.empty()) {
+		cout << "\n image is not loaded! - " << img2;
 	}
 
-	capture = cvCaptureFromCAM(vid_dev);
-	if(capture) {
-		for(;;) {
-			/* query a frame */
-			frame = cvQueryFrame(capture);
+	char output_path1[100], output_path2[100];
 
-			if(!frame.empty()) {
-				/* gray conversion */
-				cvtColor(frame, frame_gray, CV_BGR2GRAY);
-				equalizeHist(frame_gray, frame_gray);
-				face_cascade.detectMultiScale(frame_gray, faces,
-						1.1, 3, 0|CV_HAAR_SCALE_IMAGE, Size(30, 30));
+	strcpy(output_path1, "output\\");
+	strcat(output_path1, img1);
+	strcpy(output_path2, "output\\");
+	strcat(output_path2, img2);
 
-				for(size_t i = 0; i < faces.size(); i++) {
-					Point lefttop(faces[i].x,faces[i].y);
-					Point rightbottom(faces[i].x + faces[i].width,
-							faces[i].y + faces[i].height);
-					/* Draw yellow color rectangle over the face */
-					rectangle(frame, lefttop, rightbottom,
-							Scalar(0, 255, 255), 1, 8, 0);
-				}
-				/* display */
-				imshow("Demo : Face-detection", frame);
-			}
-			c = waitKey(10);
-			/* Quit if ESC key is pressed */
-			if(c == ESC) {
-				break;
-			}
-		}
-	}
+
+	detectFaces(image1, output_path1);
+	detectFaces(image2, output_path2);
+
+
 	return 0;
 }
+
+void detectFaces(Mat img, char output_path[]) {
+
+	vector<Rect> faces;
+
+	faceDetection.detectMultiScale(img, faces);
+
+	for (int i = 0; i < faces.size(); i++) {
+		Point pt1(faces[i].x, faces[i].y);
+		Point pt2((faces[i].x + faces[i].height), (faces[i].y + faces[i].width));
+
+		rectangle(img, pt1, pt2, Scalar(0, 255, 0), 2, 8, 0);
+	}
+
+	imwrite(output_path, img);
+
+	cout << "\n Image is detected successfully!";
+
+}
+
+
+
